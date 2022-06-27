82de4140ac5533800e00fbc62b50c9d16f1c296d
CarlosM0918 <107224306+CarlosM0918@users.noreply.github.com>
2022-06-27 10:53:32 -0500

    .

--- src/test/java/com/ti/pages/SearchPage.java
+++ src/test/java/com/ti/pages/SearchPage.java
@@ -1,13 +1,11 @@
 package com.ti.pages;
 
-import org.omg.PortableServer.THREAD_POLICY_ID;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.List;
